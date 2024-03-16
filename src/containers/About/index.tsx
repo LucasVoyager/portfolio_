d2b5f63be491cf77edc34dd2d3659c1ebee221be
Lucas barros <fallenagamesytb@gmail.com>
2024-03-15 22:56:57 -0300

    build: definindo componentes e criando estrutura do site

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,14 @@
+import Title from '../../components/Title'
+
+const About = () => (
+  <section>
+    <Title>Sobre mim:</Title>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores animi
+      unde ullam repellat? Magnam, quia ullam nobis fugiat nulla delectus
+      corrupti excepturi ab officia aut harum eius culpa earum. Facilis.
+    </p>
+  </section>
+)
+
+export default About
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/LucasVoyager.png" />
+    <Title>Lucas Barros</Title>
+  </aside>
+)
+
+export default Sidebar
