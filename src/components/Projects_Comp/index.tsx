4d570119656d93fe12377c6c868427e8628ab35d
Lucas barros <fallenagamesytb@gmail.com>
2024-03-17 19:31:36 -0300

    feat: desenvolvendo a section projects

---
+++ src/components/Projects_Comp/index.tsx
@@ -0,0 +1,13 @@
+import Paragraph from '../Paragraph'
+import Title from '../Title'
+import { Card, LinkButton } from './styles'
+
+const Projects_comp = () => (
+  <Card>
+    <Title>Projeto lista de tarefas</Title>
+    <Paragraph typep="secondary">Lista de tarefas feita com vue js</Paragraph>
+    <LinkButton>Visualizar</LinkButton>
+  </Card>
+)
+
+export default Projects_comp
--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -1,8 +1,36 @@
+import Projects_comp from '../../components/Projects_Comp'
 import Title from '../../components/Title'
+import { List } from './styles'
 
 const Projects = () => (
   <section>
     <Title fontSize={16}>Projetos</Title>
+    <List>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+      <li>
+        <Projects_comp></Projects_comp>
+      </li>
+    </List>
   </section>
 )
 
